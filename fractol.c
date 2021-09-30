5c3ca97e2be7428fe750a5252524a2bb85e4d8ce
Jose pedro Santos <zepedroabsantos@gmail.com>
2021-09-30 18:45:18 +0100

    Very early stages

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: josantos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 15:59:31 by josantos          #+#    #+#             */
-/*   Updated: 2021/09/30 15:20:21 by josantos         ###   ########.fr       */
+/*   Updated: 2021/09/30 18:44:31 by josantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 int	main()
 {
 	void	*mlx;
+	t_data	img;
 	void	*mlx_win;
 
 	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, 1920, 1080);
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Banana");
 	mlx_loop(mlx);
 }
--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: josantos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 18:14:00 by josantos          #+#    #+#             */
-/*   Updated: 2021/09/30 18:23:36 by josantos         ###   ########.fr       */
+/*   Updated: 2021/09/30 18:38:10 by josantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 # include <stdlib.h>
 # include "../mlx/mlx.h"
 # include "../libft/libft.h"
+
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		endian;
+}	t_data;
 
 void	fractol();
 
