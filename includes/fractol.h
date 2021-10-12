af9cd41cae36bdab95bc41f3b1565abee5e5678a
Jose pedro Santos <zepedroabsantos@gmail.com>
2021-10-12 18:59:28 +0100

    Initial part done

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,26 +6,52 @@
 /*   By: josantos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 18:14:00 by josantos          #+#    #+#             */
-/*   Updated: 2021/09/30 18:38:10 by josantos         ###   ########.fr       */
+/*   Updated: 2021/10/12 17:48:02 by josantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+# define ESC_KEY 53
+# define ZOOM_IN  5
+# define ZOOM_OUT  4
+# define PRECISION_UP 69
+# define PRECISION_DOWN 78
+
+# ifndef WIDTH
+#  define WIDTH 1920
+# endif
+
+# ifndef HEIGHT
+#  define HEIGHT 1080
+# endif
+
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 # include "../mlx/mlx.h"
 # include "../libft/libft.h"
 
-typedef struct	s_data
+typedef struct	s_img
 {
 	void	*img;
 	char	*addr;
-	int		bits_per_pixel;
+	int		bpp;
+	int		line_length;
 	int		endian;
+}	t_img;
+
+typedef struct	s_data
+{
+	void	*mlx;
+	void	*win;
+	t_img 	*img;
+	char	*fractal_name
 }	t_data;
 
 void	fractol();
 
 #endif
+
+
